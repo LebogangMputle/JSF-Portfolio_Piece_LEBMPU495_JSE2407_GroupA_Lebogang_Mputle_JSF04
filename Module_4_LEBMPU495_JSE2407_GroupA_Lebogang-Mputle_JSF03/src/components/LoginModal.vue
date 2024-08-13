224d8c91063a41b7c664ebca50dd0acbcfa0a2c6
LebogangMputle <lebozikalala@gmail.com>
2024-08-13 09:08:08 +0200

    "Added password visibility toggle to LoginModal component"

--- Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/components/LoginModal.vue
+++ Module_4_LEBMPU495_JSE2407_GroupA_Lebogang-Mputle_JSF03/src/components/LoginModal.vue
@@ -9,7 +9,21 @@
         </div>
         <div class="mb-4">
           <label for="password" class="block mb-2">Password</label>
-          <input type="password" id="password" v-model="password" class="w-full p-2 border rounded">
+          <div class="relative">
+            <input
+              :type="passwordVisible ? 'text' : 'password'"
+              id="password"
+              v-model="password"
+              class="w-full p-2 border rounded"
+            />
+            <button
+              type="button"
+              @click="togglePasswordVisibility"
+              class="absolute inset-y-0 right-2 flex items-center text-sm text-gray-600"
+            >
+              {{ passwordVisible ? 'Hide' : 'Show' }}
+            </button>
+          </div>
         </div>
         <div class="flex justify-end">
           <button type="button" @click="$emit('cancel')" class="bg-gray-200 text-gray-800 py-2 px-4 rounded mr-2">Cancel</button>
@@ -25,10 +39,14 @@ export default {
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      passwordVisible: false // State to track password visibility
     };
   },
   methods: {
+    togglePasswordVisibility() {
+      this.passwordVisible = !this.passwordVisible; // Toggle the password visibility state
+    },
     async login() {
       try {
         const response = await fetch('https://fakestoreapi.com/auth/login', {
